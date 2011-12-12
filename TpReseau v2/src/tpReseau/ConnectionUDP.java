ffb03a54d8f2f5cc57cdaf44178e2f8b718d5ce6
rgicquel <rgicquel@9449b301-a70c-4e59-97b0-86f01f209cd8>
2011-12-12 22:08:09 +0000

    git-svn-id: https://shareif.insa-lyon.fr/svn/rg.tpreseau1@14 9449b301-a70c-4e59-97b0-86f01f209cd8

--- TpReseau v2/src/tpReseau/ConnectionUDP.java
+++ TpReseau v2/src/tpReseau/ConnectionUDP.java
@@ -1,6 +1,26 @@
 package tpReseau;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 public class ConnectionUDP {
+	
+	public ConnectionUDP() {
+		int port;
+		InetAddress address;
+		DatagramSocket socket = null;
+		DatagramPacket packet;
+		byte[] buf = new byte[256];
+		
+		try {
+			socket = new DatagramSocket();
+			packet = new DatagramPacket();
+		} catch (SocketException e) {
+			e.printStackTrace();
+		}
+	}
 	
 	public static void main(String[] args) {
 		
